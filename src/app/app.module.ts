0bada252637067d057ad3345da252c20ecb33041
Everton Rocha <imevertonrch@gmail.com>
2024-11-17 05:00:15 -0300

    interceptando as requisições para a API em memória

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// para interceptar as requisições em memória
+// para chamadas externas remover esse módulo
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from './in-memory-database';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase )
   ],
   providers: [],
   bootstrap: [AppComponent]
